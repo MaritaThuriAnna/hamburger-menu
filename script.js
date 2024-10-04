c4d714b4dac08ad2b4b97df80ab9bca5a83efa59
Marita-Thuri Anna <maritathurianna@gmail.com>
2024-10-04 11:54:04 +0300

    fix js

--- script.js
+++ script.js
@@ -10,12 +10,11 @@ const menuItems = document.querySelectorAll('.menu li a');
 // event listener pentru click
 menuItems.forEach(item => {
     item.addEventListener('click', function(e) {
-        // Prevenim comportamentul implicit al link-ului doar dacă există un submenu
         const submenu = this.nextElementSibling;
 
         if (submenu && submenu.classList.contains('submenu')) {
-            e.preventDefault(); // Previne click-ul pe linkurile cu submenu
-            submenu.classList.toggle('open'); // Toggle submenu visibility
+            e.preventDefault();
+            submenu.classList.toggle('open');
         }
     });
 });
